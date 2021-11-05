260615ab1e6acf039a77e3d3221c421031a04a55
achmad syarif <ach.sya03@gmail.com>
2021-11-06 05:53:57 +0700

    Update User

--- app/Http/Controllers/User/WebController.php
+++ app/Http/Controllers/User/WebController.php
@@ -7,24 +7,24 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-    public function updateDeviceID(Request $request)
-    {
-        if(!$uuid=$request->email){
-            return response()->json(['message'=>'Failed','info'=>"Email Tidak Sesuai"]);
-        }
+    // public function updateDeviceID(Request $request)
+    // {
+    //     if(!$uuid=$request->email){
+    //         return response()->json(['message'=>'Failed','info'=>"Email Tidak Sesuai"]);
+    //     }
 
-        if(count($user_upd = Models\User::where('email',$request->email)->get())==0){
-            return response()->json(['message'=>'Failed','info'=>"Email Tidak Sesuai"]);
-        }
+    //     if(count($user_upd = Models\User::where('email',$request->email)->get())==0){
+    //         return response()->json(['message'=>'Failed','info'=>"Email Tidak Sesuai"]);
+    //     }
 
        
-        $user_upd = Models\User::where('email',$request->email)
-                ->update([
-                    'device_id' => $request->device_id,
-                ]);
+    //     $user_upd = Models\User::where('email',$request->email)
+    //             ->update([
+    //                 'device_id' => $request->device_id,
+    //             ]);
 
-        return response()->json(['message'=>'Success','info'
-        => 'Proses Update Device ID Berhasil']);
-    }
+    //     return response()->json(['message'=>'Success','info'
+    //     => 'Proses Update Device ID Berhasil']);
+    // }
 
 }
