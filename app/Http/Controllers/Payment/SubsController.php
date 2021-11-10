e0b5f9c60a5b66d0fc07f07c24d9efcd29286d31
achmad syarif <ach.sya03@gmail.com>
2021-11-11 02:07:41 +0700

    Update Subs

--- app/Http/Controllers/Payment/SubsController.php
+++ app/Http/Controllers/Payment/SubsController.php
@@ -679,7 +679,7 @@ class SubsController extends Controller
 
 		
         $result['tgl_daftar'] = $subs[0]->tgl_subs;
-        $result['tgl_akhir'] = $subs[0]->tgl_akhir_bayar;
+        $result['tgl_akhir'] = (new \DateTime($subs[0]->tgl_subs))->modify('+'.(30*$subs[0]->packet->lama_paket).' day')->format('Y-m-d');
         $result['kode_referal'] = $kode;
         $result['nama_referal'] = $nama;
         $result['packet'] = $packet;
