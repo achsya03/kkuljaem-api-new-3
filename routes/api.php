7337c059269169d050b6686173ff7b3b0bfab2ab
achmad syarif <ach.sya03@gmail.com>
2021-11-24 16:08:21 +0700

    Update Force

--- routes/api.php
+++ routes/api.php
@@ -146,9 +146,9 @@ Route::group(['prefix' => 'api/force'], function () {
     // Route::get('words/url',      [Helper\ForceController::class,'forceWordUrl']);
     // Route::get('post/url',      [Helper\ForceController::class,'forcePostUrl']);
     // Route::get('banner/url',      [Helper\ForceController::class,'forceBannerUrl']);
-    Route::get('question/url',      [Helper\ForceController::class,'forceBannerUrl']);
-    Route::get('option/url',      [Helper\ForceController::class,'forceBannerUrl']);
-    Route::get('mentor-detail/url',      [Helper\ForceController::class,'forceBannerUrl']);
+    Route::get('question/url',      [Helper\ForceController::class,'forceMentorDetailUrl']);
+    Route::get('option/url',      [Helper\ForceController::class,'forceQuestionUrl']);
+    Route::get('mentor-detail/url',      [Helper\ForceController::class,'forceOptionUrl']);
 });    
 Route::group(['prefix' => 'api/user/packet'], function () {
     Route::get('/',         [Packet\PacketController::class,'allData']);
