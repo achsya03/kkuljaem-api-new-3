783260ab8d89e972312975099a8b595f46bd03ea
Achmad Syarif. Abdullah <achmad.syarif@ecampuz.com>
2022-03-09 22:10:52 +0700

    Update Admin User

--- routes/api.php
+++ routes/api.php
@@ -497,7 +497,7 @@ Route::group(['prefix' => 'api/admin/user'], function () {
                 })
                 ->addColumn('action', function ($student) {
                     $actionBtn = '<a href="http://admin.kkuljaemkoreanapp.com/akun_pengguna/siswa/detail?id='.$student->user_uuid.'" class="info btn btn-info btn-sm">Rincian</a>
-                                <a href="http://admin.kkuljaemkoreanapp.com/akun_pengguna/siswa/edit?id='.$student->user_uuid.'" class="edit btn btn-success btn-sm">Edit</a> 
+                                <a href="http://admin.kkuljaemkoreanapp.com/akun_pengguna/siswa/edit?token='.$student->user_uuid.'" class="edit btn btn-success btn-sm">Edit</a> 
                                 <a href="#" onclick="hapus(https://kkuljaem-api-new-3-ft4mz.ondigitalocean.app/api/admin/user/student?token='.$student->user_uuid.'" class="delete btn btn-danger btn-sm">Delete</a>';
                     return $actionBtn;
                 })
